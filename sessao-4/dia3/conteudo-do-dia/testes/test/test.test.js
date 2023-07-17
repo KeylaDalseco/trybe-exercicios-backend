e33aed2ea478ec5789b2f2c7c3907b56af1d46c9
Keyla Dalseco <keylacostadalseco@gmail.com>
2023-07-17 10:51:32 -0300

    feat: Conteúdo do dia 2 e início do dia 3

---
+++ sessao-4/dia3/conteudo-do-dia/testes/test/test.test.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+const calculaSituacao = require('../index.js');
+
+describe('Quando a média for menor que 7', function () {
+  it('retorna "reprovação"', function () {
+    const resposta = calculaSituacao(4);
+
+    expect(resposta).to.be.equals('reprovação');
+  });
+});
