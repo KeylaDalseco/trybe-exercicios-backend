c314ad576e7f99b61fd8f90d73cf8f056210efa4
Keyla Dalseco <keylacostadalseco@gmail.com>
2023-09-22 14:01:35 -0300

    feat: atividades do dia 4 e 5

---
+++ seção-9/dia4/exercicio-do-dia-4/src/models/interfaces/index.ts
@@ -0,0 +1,11 @@
+export { IPlant, INewPlant } from './IPlant';
+export {
+  IModel, IModelDelete, IModelReader, IModelWriter,
+} from './IModel';
+export { default as IOpsInfo } from './IOpsInfo';
+export {
+  IService,
+  IServiceReader,
+  IServiceWriter,
+  IServiceDelete,
+} from './IService';
